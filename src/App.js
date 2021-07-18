9d1047b88810f1963e4059a307d6b9aed34b8c31
Henrique Saraiva <saraivahe@icloud.com>
2021-07-18 23:43:10 +0100

    Search Field State

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: '',
     };
   }
 
@@ -22,6 +23,11 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
+        <input
+          type='search'
+          placeholder='search monsters'
+          onChange={(e) => this.setState({ searchField: e.target.value })}
+        />
         <CardList monsters={this.state.monsters} />
       </div>
     );
