e695b0b77036388d61140afeeee82cc7bc19f234
Henrique Saraiva <saraivahe@icloud.com>
2021-07-18 20:58:32 +0100

    Dynamic Content

--- src/App.js
+++ src/App.js
@@ -8,20 +8,20 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Hello Saraiva',
+      monsters: [
+        { name: 'Frankenstein', id: 'id1' },
+        { name: 'Dracula', id: 'id2' },
+        { name: 'Zombie', id: 'id3' },
+      ],
     };
   }
 
   render() {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: 'Hello again' })}>
-            Change Text
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
